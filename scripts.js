4ec6b9377f30938b5f6bab90761c499022f34427
PadoSensei <44815835+PadoSensei@users.noreply.github.com>
2020-05-22 16:50:33 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,22 +1,64 @@
-// Get DOM targets, add listeners
+// Hamburger Menu Toggle script
 
-const issBtn = document.getElementById("iss-btn");
-document.getElementById("cocktail-btn").addEventListener("click", getDrink);
-document.getElementById("iss-btn").addEventListener("click", getISS);
-document.getElementById("joke-btn").addEventListener("click", getMoreJokes);
-//document.getElementById("kayne-btn").addEventListener("click", getKanye);
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-document.getElementById("home-btn").addEventListener("click", function () {
-  document.getElementById("thing-title").innerHTML =
-    "Welcome to Podge's Lodge of HodgePodge!";
-  document.getElementById("thing-desc").innerHTML =
-    "Back at the start. What do you want to do?";
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
 
-// Random Cocktail - getDrink()
-const drink_url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+// Gallery grid and opaque selection
+const current = document.querySelector("#current");
+const imgs = document.querySelector(".imgs");
+const img = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+// Set first img opacity
+img[0].style.opacity = opacity;
+imgs.addEventListener("click", imgClick);
+
+function imgClick(e) {
+  // Reset the opacity
+  img.forEach((img) => (img.style.opacity = 1));
+
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+
+  // Add fade in class
+  current.classList.add("fade-in");
+
+  // Remove fade-in class after .5 seconds
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+
+  // Change the opacity to opacity var
+  e.target.style.opacity = opacity;
+}
+
+// Joke JSON and display
+// const jokeBtn = document.getElementById("jokeBtn");
+// jokeBtn.addEventListener("click", getMoreJokes);
+
+async function getMoreJokes() {
+  const response = await fetch("./jokes.json");
+  const data = await response.json();
+  console.log(data);
+  const output = random_item(data.jokes);
+  console.log(output);
+
+  document.getElementById("joke-display").innerHTML = `${output.joke}`;
+}
+
+function random_item(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+// // Random Cocktail - getDrink()
+//const drink_url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 async function getDrink() {
-  const response = await fetch(drink_url);
+  //const drink_url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  const response = await fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  );
   const data = await response.json();
   console.log(data);
   const drink = data.drinks[0].strDrink;
@@ -58,71 +100,16 @@ async function getDrink() {
   }
   console.log(measureList);
 
-  document.getElementById("thing-title").innerHTML = "Random Cocktail!";
-  const img = document.createElement("img");
-  img.src = image;
-  document.getElementById("thing-title").appendChild(img);
+  const pic = document.getElementById("cocktail-pic");
+  pic.src = image;
 
   document.getElementById(
-    "thing-desc"
+    "cocktail-display"
   ).innerHTML = `Try a ${drink}.  It's served in a ${glass}. ${instruct}`;
 
   for (let x = 0; x < ingredList.length; x++) {
     const ingredients = document.createElement("p");
     ingredients.innerText = measureList[x] + ": " + ingredList[x];
-    document.getElementById("thing-desc").appendChild(ingredients);
+    document.getElementById("cocktail-display").appendChild(ingredients);
   }
-}
-
-// Random Joke
-const joke_url =
-  "https://sv443.net/joeapi/v2/joke/Miscellaneous?blacklistFlags=nsfw,religious,political,racist,sexist&type=single";
-async function getJoke() {
-  const response = await fetch(joke_url);
-  const data = await response.json();
-  console.log(data.joke);
-  document.getElementById("thing-title").innerHTML = "Random Joke!";
-  document.getElementById("thing-desc").innerHTML = `${data.joke}`;
-}
-
-const moreJokes_url = "./jokes.json";
-
-async function getMoreJokes() {
-  const response = await fetch(moreJokes_url);
-  const data = await response.json();
-  console.log(data);
-  const output = random_item(data.jokes);
-  console.log(output);
-  document.getElementById("thing-desc").innerHTML = `${output.joke}`;
-}
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
-// Random Kayne
-const kanye_url = "https://api.kanye.rest";
-async function getKanye() {
-  const response = await fetch(kanye_url);
-  const data = await response.json();
-  console.log(data);
-  document.getElementById("thing-title").innerHTML =
-    "Random Kanye! (I take no responsibility for what he says...)";
-  document.getElementById("thing-desc").innerHTML = `${data.quote}`;
-}
-
-// Where's the ISS?
-const iss_url = "https://api.wheretheiss.at/v1/satellites/25544";
-async function getISS() {
-  const response = await fetch(iss_url);
-  const data = await response.json();
-  const { longitude, latitude } = data;
-  console.log(data);
-  console.log(longitude);
-  console.log(latitude);
-  document.getElementById("thing-title").innerHTML =
-    "Where's the International Space Station right now?";
-  document.getElementById(
-    "thing-desc"
-  ).innerHTML = `Its longitude is ${longitude} and its latitude is ${latitude}. I'm working on a map to display this.`;
 }
