e43a794cbea65cc4f3a00b8eecd4ffb770b6301b
Sensei_P <padraigo.donoghue@gmail.comgit config --global user.name Sensei_Pgit config --global user.email padraigo.donoghue@gmail.com>
2023-07-29 15:25:07 +0100

    Refactor scripts file and remove duplicate jpeg

--- scripts.js
+++ scripts.js
@@ -15,6 +15,43 @@ document.addEventListener("DOMContentLoaded", (e) => {
   }, 2000);
 });
 
+const vase_pics = [
+  "./Art/Vase_Dad.jpeg",
+  "./Art/Vase_Kates.jpeg",
+  "./Art/Vase_Lu.jpeg",
+  "./Art/Vase_Maria.jpeg",
+  "./Art/Vase_Mum.jpeg",
+  "./Art/Vase_P.png",
+  "./Art/Vase_Steve.jpeg",
+  "./Art/Vase_Orig.jpeg",
+];
+
+const swan_pics = ["swan_Orig.jpeg"];
+
+const coast_pics = [
+  "./Art/Coast_Dad.jpeg",
+  "./Art/Coast_Kates.jpeg",
+  "./Art/Coast_Maria.jpeg",
+  "./Art/Coast_Steve.jpeg",
+  "./Art/Coast_Orig.jpeg",
+  "Art/Coast_P.jpg",
+];
+
+const garden_pics = [
+  "./Art/Seat_P.jpeg",
+  "./Art/Seat_Steve.jpeg",
+  "./Art/Seat_Dad.jpeg",
+  "./Art/Seat_Kates.jpeg",
+  "./Art/Seat_Maria.jpeg",
+  "./Art/Seat_Orig.jpeg",
+];
+
+const canal_pics = [
+  "./Art/Canal_Dad.jpeg",
+  "./Art/Canal_Maria.jpeg",
+  "./Art/Canal_Steve.jpeg",
+];
+
 // Gallery grid and opaque selection
 const current = document.querySelector("#current");
 const imgs = document.querySelector(".imgs");
@@ -88,43 +125,6 @@ function swanPics() {
   removeGallery();
   // run your function on swan_pics
 }
-
-const vase_pics = [
-  "./Art/Vase_Dad.jpeg",
-  "./Art/Vase_Kates.jpeg",
-  "./Art/Vase_Lu.jpeg",
-  "./Art/Vase_Maria.jpeg",
-  "./Art/Vase_Mum.jpeg",
-  "./Art/Vase_P.png",
-  "./Art/Vase_Steve.jpeg",
-  "./Art/Vase_Orig.jpeg",
-];
-
-const swan_pics = ["swan_Orig.jpeg"];
-
-const coast_pics = [
-  "./Art/Coast_Dad.jpeg",
-  "./Art/Coast_Kates.jpeg",
-  "./Art/Coast_Maria.jpeg",
-  "./Art/Coast_Steve.jpeg",
-  "./Art/Coast_Orig.jpeg",
-  "Art/Coast_P.jpg",
-];
-
-const garden_pics = [
-  "./Art/Seat_P.jpeg",
-  "./Art/Seat_Steve.jpeg",
-  "./Art/Seat_Dad.jpeg",
-  "./Art/Seat_Kates.jpeg",
-  "./Art/Seat_Maria.jpeg",
-  "./Art/Seat_Orig.jpeg",
-];
-
-const canal_pics = [
-  "./Art/Canal_Dad.jpeg",
-  "./Art/Canal_Maria.jpeg",
-  "./Art/Canal_Steve.jpeg",
-];
 
 function removeGallery() {
   const galleryPics = document.getElementsByClassName("gallery");
